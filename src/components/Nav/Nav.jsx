06e6c10d4a23e23beec1fdd820d52c496f865f26
awwal <leyeawwal@gmail.com>
2024-07-06 14:23:46 +0100

    improve style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,89 +3,113 @@ import { Link } from "react-router-dom";
 import "./Nav.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faBriefcase,
-  faHouse,
-  faEnvelopeOpen,
-  faUser,
+	faBriefcase,
+	faHouse,
+	faEnvelopeOpen,
+	faUser,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Front = (props) => {
-  const [menu, setMenu] = useState(false);
+	const [menu, setMenu] = useState(false);
 
+	const [currentPage, setCurrentPage] = useState("home");
 
-  const toggle = () => {
-    setMenu(!menu);
-  };
+  const handlePageChange = (page) => {
+    setCurrentPage(page)
+  }
 
-  return (
-    <div className="nav">
-      <div className="navHeader">
-        <nav>
-          <div className={`hamburger ${menu ? "active" : ""}`} onClick={toggle}>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <ul
-            className="mobile-nav-list"
-            style={{ display: menu ? "block" : "none" }}
-            onClick={toggle}
-          >
-            <Link to="/">
-              <li>HOME</li>
-            </Link>
+	const toggle = () => {
+		setMenu(!menu);
+	};
 
-            <Link to="/ABOUT">
-              <li>ABOUT</li>
-            </Link>
+	return (
+		<div className="nav">
+			<div className="navHeader">
+				<nav>
+					<div
+						className={`hamburger ${menu ? "active" : ""}`}
+						onClick={toggle}
+					>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+					<ul
+						className="mobile-nav-list"
+						style={{ display: menu ? "block" : "none" }}
+						onClick={toggle}
+					>
+						<Link to="/">
+							<li>HOME</li>
+						</Link>
 
-            <Link to="/PORTFOLIO">
-              <li>PORTFOLIO</li>
-            </Link>
+						<Link to="/ABOUT">
+							<li>ABOUT</li>
+						</Link>
 
-            <Link to="/CONTACT">
-              <li>CONTACT</li>
-            </Link>
-          </ul>
-        </nav>
-      </div>
+						<Link to="/PORTFOLIO">
+							<li>PORTFOLIO</li>
+						</Link>
 
-      <div className="navHome">
-      <div className="screen-nav home-screen">
-        <Link to="/">
-          <FontAwesomeIcon
-            className={`screen-icon}`}
-            icon={faHouse}
-          />
-        </Link>
-      </div>
-      <div className="screen-nav profile-screen">
-        <Link to="/ABOUT">
-          <FontAwesomeIcon
-            className={`screen-icon}`}
-            icon={faUser}
-          />
-        </Link>
-      </div>
-      <div className="screen-nav portfolio-screen">
-        <Link to="/PORTFOLIO">
-          <FontAwesomeIcon
-            className={`screen-icon}`}
-            icon={faBriefcase}
-          />
-        </Link>
-      </div>
-      <div className="screen-nav contact-screen">
-        <Link to="/CONTACT">
-          <FontAwesomeIcon
-            className={`screen-icon}`}
-            icon={faEnvelopeOpen}
-          />
-        </Link>
-      </div>
-    </div>
-    </div>
-  );
+						<Link to="/CONTACT">
+							<li>CONTACT</li>
+						</Link>
+					</ul>
+				</nav>
+			</div>
+
+			<div className="navHome">
+				<Link
+					to="/"
+					className={`screen-nav ${currentPage === 'home' ? 'screen-active' : ''}`}
+          onClick={() => handlePageChange("home")}
+				>
+					<div>
+						<FontAwesomeIcon
+							className={`screen-icon}`}
+							icon={faHouse}
+						/>
+					</div>
+				</Link>
+				<Link
+					to="/ABOUT"
+					className={`screen-nav ${currentPage === 'about' ? 'screen-active' : ''}`}
+          onClick={() => handlePageChange("about")}
+				>
+					<div>
+						<FontAwesomeIcon
+							className={`screen-icon}`}
+							icon={faUser}
+						/>
+					</div>
+				</Link>
+				<Link
+					to="/PORTFOLIO"
+					className={`screen-nav ${currentPage === 'portfolio' ? 'screen-active' : ''}`}
+          onClick={() => handlePageChange("portfolio")}
+				>
+					<div>
+						<FontAwesomeIcon
+							className={`screen-icon}`}
+							icon={faBriefcase}
+						/>
+					</div>
+				</Link>
+				<Link
+					to="/CONTACT"
+					className={`screen-nav ${currentPage === 'contact' ? 'screen-active' : ''}`}
+          onClick={() => handlePageChange("contact")}
+				>
+					<div>
+						<FontAwesomeIcon
+							className={`screen-icon}`}
+							icon={faEnvelopeOpen}
+						/>
+					</div>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Front;
