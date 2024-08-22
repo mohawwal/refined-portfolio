3e47981fb18f39b07fd3504907af9d288b838ebd
awwal <leyeawwal@gmail.com>
2024-08-22 15:32:52 +0100

    add a new project

--- src/components/File/Projects.js
+++ src/components/File/Projects.js
@@ -1,7 +1,18 @@
 
 let data_product = [
-{
+  {
     id: 1,
+    name: "ZARMARIO E-COMMERCE",
+    description: "A full-stack e-commerce web application built using the MERN stack (MongoDB, Express, React, Node.js). The project features both client-side and server-side implementations, providing a seamless user experience. The client-side is developed with React, incorporating Redux for efficient state management and ensuring a responsive and interactive interface. The server-side, powered by Node.js and Express, handles the backend operations, including user authentication, product management, and order processing. MongoDB is used for the database, enabling flexible data storage and retrieval. The application also includes a dedicated admin panel for managing products, orders, and users.",
+    githubLink: "https://github.com/mohawwal/shopIt-API",
+    webLink: "https://zarmario.vercel.app/",
+    languages: [
+        "Node.js", "React", "Mongoose", "Express"
+    ]
+},
+
+{
+    id: 2,
     name: "E-COMMERCE",
     description: "Discover and order your favorite burgers effortlessly on our e-commerce platform. Browse, add to cart, and search for your desired burgers with ease.",
     githubLink: "",
@@ -10,7 +21,7 @@ let data_product = [
         "CSS", "ANGULAR", "TYPESCRIPT"   ]
   },
   {
-    id: 2,
+    id: 3,
     name: "TO-DO LIST",
     description: "A simple and intuitive task management app designed to streamline your daily tasks. Easily create, organize, and prioritize your to-dos with our user-friendly interface. Stay on top of your tasks and boost your productivity with TO-DO List.",
     githubLink: "",
@@ -19,7 +30,7 @@ let data_product = [
         "HTML", "CSS", "FIREBASE", "JAVASCRIPT" ]
   },
   {
-    id: 3,
+    id: 4,
     name: "SNACK GAME",
     description: "A fun and addictive game to satisfy your snack cravings! Dive into this JavaScript-powered game where you can challenge yourself to collect snacks while avoiding obstacles. Play now and see how many snacks you can collect!",
     githubLink: "",
@@ -29,7 +40,7 @@ let data_product = [
   ]  
   },
   {
-    id: 4,
+    id: 5,
     name: "TIC TAC TOE",
     description: "A classic game reimagined for the digital age. Built with React, this modern twist on the traditional game offers a seamless gaming experience. Challenge your friends or play against the computer in this timeless game of strategy and skill.",
     githubLink: "",
