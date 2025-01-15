4b95827223b47508d54af8faa38a2405f8e56850
awwal <leyeawwal@gmail.com>
2025-01-15 04:35:37 +0100

    add dashboard project

--- src/components/File/Projects.js
+++ src/components/File/Projects.js
@@ -1,6 +1,15 @@
 let data_product = [
 	{
 		id: 0,
+		name: "SALES DETAL DASHBOARD",
+		description:
+			"A sales data website that updates sales, connect company staffs together and improve task management",
+		githubLink: "https://github.com/mohawwal/salesTrack",
+		webLink: "https://sales-detal.vercel.app/dashboard",
+		languages: ["vite", "React", "shadcn", "Tailwind"],
+	},
+	{
+		id: 1,
 		name: "SOCIAL MEDIA WEBAPP",
 		description:
 			"A full-stack social media webapp where users can add post, like, rt & bookmark posts, and send direct messages (DM)",
@@ -9,7 +18,7 @@ let data_product = [
 		languages: ["Node.js", "React", "MySQL", "Express"],
 	},
 	{
-		id: 1,
+		id: 2,
 		name: "ZARMARIO E-COMMERCE APP",
 		description:
 			"A full-stack web app built using MongoDB, Express, React, Node.js. The project features both client & server-side implementations, providing a seamless user experience. The client-side incorporates Redux for efficient state management. The server-side, handles the backend operations, including user authentication, product management, and order processing.",
@@ -19,7 +28,7 @@ let data_product = [
 	},
 
 	{
-		id: 2,
+		id: 3,
 		name: "E-COMMERCE",
 		description:
 			"Discover and order your favorite burgers effortlessly on our e-commerce platform. Browse, add to cart, and search for your desired burgers with ease.",
@@ -28,7 +37,7 @@ let data_product = [
 		languages: ["CSS", "ANGULAR", "TYPESCRIPT"],
 	},
 	{
-		id: 3,
+		id: 4,
 		name: "TO-DO LIST",
 		description:
 			"A simple and intuitive task management app designed to streamline your daily tasks. Easily create, organize, and prioritize your to-dos with our user-friendly interface. Stay on top of your tasks and boost your productivity with TO-DO List.",
@@ -37,7 +46,7 @@ let data_product = [
 		languages: ["HTML", "CSS", "FIREBASE", "JAVASCRIPT"],
 	},
 	{
-		id: 4,
+		id: 5,
 		name: "SNACK GAME",
 		description:
 			"A fun and addictive game to satisfy your snack cravings! Dive into this JavaScript-powered game where you can challenge yourself to collect snacks while avoiding obstacles. Play now and see how many snacks you can collect!",
@@ -46,7 +55,7 @@ let data_product = [
 		languages: ["JAVASCRIPT", "CSS", "HTML"],
 	},
 	{
-		id: 5,
+		id: 6,
 		name: "TIC TAC TOE",
 		description:
 			"A classic game reimagined for the digital age. Built with React, this modern twist on the traditional game offers a seamless gaming experience. Challenge your friends or play against the computer in this timeless game of strategy and skill.",
